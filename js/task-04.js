8be068fbbc6a8baf056f2214def3c4209c26a50b
ViktoriiaYakaba <yakaba@bk.ru>
2023-12-13 20:01:14 +0100

    update task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,22 @@
+const formLogin = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+       return alert('All form fields must be filled in');
+  }
+    const values = {
+        email: email.trim(),
+        password: password.trim(),
+    };
+    console.log(values);
+    
+    form.reset();
+}
+formLogin.addEventListener("submit", handleSubmit);
+
+    
