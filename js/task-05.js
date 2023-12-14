f5b2c7fb2d09b1f1de27ff411b411610c9c4ebcc
ViktoriiaYakaba <yakaba@bk.ru>
2023-12-14 20:05:50 +0100

    update

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,16 @@
+const button = document.querySelector(" button.change-color");
+const spanColor = document.querySelector(".color");
+const widget = document.querySelector(".widget");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    widget.style.backgroundColor = randomColor;
+    spanColor.textContent = randomColor;
+});
